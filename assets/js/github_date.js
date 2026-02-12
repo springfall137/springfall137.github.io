14339a79d59c62a1ad9576ba425b6751a4375220
DJ <majjayong202@gmail.com>
2026-02-12 23:04:18 +0900

    Update URLs and email: underthelights -> springfall137, email -> clairda@snu.ac.kr

--- assets/js/github_date.js
+++ assets/js/github_date.js
@@ -5,7 +5,7 @@ const gh_token3 = "uOHGVlp1zmg18SIyh2STbOa";
 let headers = new Headers();
 headers.append('Authorization', `token ${gh_token1 + gh_token2 + gh_token3}`);
 
-fetch("https://api.github.com/repos/springfall137/springfall137.github.io/branches/main", {method:'GET', headers: headers})
+fetch("https://api.github.com/repos/underthelights/underthelights.github.io/branches/main", {method:'GET', headers: headers})
 .then(response => response.json())
 .then(data => {
   let date = new Date(data.commit.commit.committer.date);
